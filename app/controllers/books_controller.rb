68b7092a0fedecfab9702c612aa7da8bac433b45
Anita Perala <anitaperala@gmail.com>
2015-08-17 14:06:47 -0400

    Add books controller methods, skeleton model, skeleton new, show, index views for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,34 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    #Admin if statement to be added
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: "Book was successfully added"
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn, :pub_date, :genre, :format, :publisher, :summary)
   end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
 
+  let(:test_book){ Book.create title: "Test Book",
+                                author: "Author Name",
+                                isbn: 1234, 
+                                pub_date: 2008, 
+                                genre: "fiction",
+                                format: "pb",
+                                publisher: "Test Pub",
+                                summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                Dolor, asperiores neque porro."
+                    }
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -9,4 +19,12 @@ RSpec.describe BooksController, type: :controller do
     end
   end
 
+  describe 'GET #new' do
+    it "assigns a new book to @book variable" do
+      get :new
+      book = test_book
+      expect(book).to be_an_instance_of Book
+    end
+  end
+  
 end
