1ae7a02bb31805c15fff1a315923117959353dd3
Anita Perala <anitaperala@gmail.com>
2015-08-18 15:04:42 -0400

    Add place edit view, extract form to partial view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,10 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def show
-    @place = Place.find(params[:id])
   end
 
   def new
@@ -21,15 +22,21 @@ class PlacesController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @place.update(place_params)
+      redirect_to @place, notice: "Location was successfully updated"
+    else
+      render :edit
+    end
   end
 
-
   private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
   def place_params
     params.require(:place).permit(:name, :region, :latitude, :longitude)
