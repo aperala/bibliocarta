740b4b082fbc36decfb7d0ca986997779435c030
Anita Perala <anitaperala@gmail.com>
2015-08-20 17:16:38 -0400

    Add Paperclip gem, add avatar to user create and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params 
+    
+    
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,9 @@ class Book < ActiveRecord::Base
   validates :isbn,   :isbn_format => true
   validates_presence_of :title
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def add(place)
     places << place
   end
