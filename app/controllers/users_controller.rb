47d89bc9ee854039e5b39a77893c58d83422873f
Anita Perala <anitaperala@gmail.com>
2015-08-23 00:11:15 -0400

    wip links on places popup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,34 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @geojsonshow = Array.new
+
+    @user.places.each do |place|
+      @geojsonshow << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [place.longitude, place.latitude]
+          },
+          properties: {
+            id: place.id,
+            name: place.name,
+            :'marker-color' => '#e74c3c',
+            :'marker-symbol' => 'circle',
+            :'marker-size' => 'small'
+        }
+      }
+    end
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojsonshow }
+    end
+
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :location)
+  end
 end
