abf38e7398ad62f9b94b7ba2f2ac35447b31586e
Anita Perala <anitaperala@gmail.com>
2015-08-26 13:03:46 -0400

    Updates index map to show only places where books >= 1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,21 +8,23 @@ class PlacesController < ApplicationController
     @geojson = Array.new
 
     @places.each do |place|
-      @geojson << {
-        type: 'Feature',
-        geometry:{
-          type: 'Point',
-          coordinates: [place.longitude, place.latitude]
-          },
-          properties: {
-            id: place.id,
-            name: place.name,
-            books: place.books.length,
-            :'marker-color' => '#29A329',
-            :'marker-symbol' => 'library',
-            :'marker-size' => 'small'
-          }
-      }
+      if place.books.length >= 1
+        @geojson << {
+          type: 'Feature',
+          geometry:{
+            type: 'Point',
+            coordinates: [place.longitude, place.latitude]
+            },
+            properties: {
+              id: place.id,
+              name: place.name,
+              books: place.books.length,
+              :'marker-color' => '#29A329',
+              :'marker-symbol' => 'library',
+              :'marker-size' => 'small'
+            }
+        }
+      end
     end
 
     respond_to do |format|
