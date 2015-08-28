52561b9053461cbde000cf7af8bcdea150967f37
Anita Perala <anitaperala@gmail.com>
2015-08-27 21:45:10 -0400

    Fixes #23 book places form added

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :user_books, only: [:create]
+  resources :book_places, only: [:new, :create]
   # Example resource route with options:
   #   resources :products do
   #     member do
